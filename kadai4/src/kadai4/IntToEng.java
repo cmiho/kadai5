bbb04964a893caec6ef3374c8257da24337fcdb9
g14902cm <g14902cm@s30433.tsuda.ac.jp>
2016-05-13 11:44:24 +0900

    999までできるよ

--- kadai4/src/kadai4/IntToEng.java
+++ kadai4/src/kadai4/IntToEng.java
@@ -20,16 +20,22 @@ public class IntToEng {
 
 		String digit[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 		String exactly[] = { "", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "ninety",
-				"one-hundred" };
+				"hundred" };
 		String teen[] = { "", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
 				"nineteen" };
 
+		
 		if (0 <= n && n < 10)
 			answer = digit[n];
 		else if (n % 10 == 0)
 			answer = exactly[n / 10];
 		else if (10 < n && n < 20)
 			answer = teen[n - 10];
+		else if(0 < n && n < 100)
+			answer = exactly[n/10] +"-" + digit[n%10];
+		else if(n > 100 && n < 1000)
+			answer = digit[n/100] +" hundred and "+ exactly[(n - (n/100) * 100) / 10] +"-"+digit[(n%100)%10];
+		
 
 		return answer;
 	}
--- kadai4/src/kadai4/IntToEngTest.java
+++ kadai4/src/kadai4/IntToEngTest.java
@@ -15,9 +15,25 @@ public class IntToEngTest {
 		assertEquals("zero", s);
 
 	}
-	/*
-	 * @Test public void さんじゅう() { String s =IntToEng.translateEng(30);
-	 * assertEquals("thirty",s); }
-	 */
+	
+	 @Test public void さんじゅう() { String s =IntToEng.translateEng(30);
+	 assertEquals("thirty",s); }
+	 
+	 @Test
+	 public void じゅうに() {
+		 String s = IntToEng.translateEng(12);
+		 assertEquals("twelve", s);
+	 }
+	 
+	 @Test
+	 public void さんじゅうに(){
+		 String s = IntToEng.translateEng(32);
+		 assertEquals("thirty-two", s);
+	 }
+	 @Test
+	 public void ごひゃくさんじゅうに(){
+		 String s = IntToEng.translateEng(532);
+		 assertEquals("five hundred and thirty-two", s);
+	 }
 
 }
